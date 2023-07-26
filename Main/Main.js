11342ac56f93dc2f5798c201381fba75821e9b99
Deaf-Web-Admin <linux.user@techie.com>
2023-07-26 12:26:33 +0200

    Proyecto Concluido?

--- Main/Main.js
+++ Main/Main.js
@@ -3,24 +3,70 @@ import "./Main.css";
 const HTMLdeInfo = () => {
 	return `
   <div class="container">
-    <p id="Temperatura">32</p>
-    <p id="CountryName">Belize</p>
+  
     </div>
   `;
 };
 
 const HTMLdeMapa = () => {
 	return `
-  mapas
+  <div class="container2">
+  
+    </div>
   
   `;
 };
-
+//ESTA FUNCION SE ENCARGA DE BUSCAR DATOS EN LA API
+const getRealTimeData = async (city) => {
+	//Hacemos la petición de los datos crudos, y hemos cambiado el valor de location por el argumento que le pasaremos a la función getRealTimeData
+	const data = await fetch(
+		`https://api.tomorrow.io/v4/weather/realtime?location=${city}&apikey=xASmtqdrf0smIvzBngVZDN9vPbfOyd6X`
+	);
+	//Parseamos los datos a json
+	const dataJSON = await data.json();
+	printData(dataJSON);
+};
+//ESTA FUNCION SE ENCARGA DE PINTAR LOS DATOS
+const printData = (data) => {
+	//Recuperamos el contendor
+	const container = document.querySelector(".divisor4");
+	const container2 = document.querySelector(".divisor3");
+	//Vamos a parsear la fecha en tipo fecha para poder utilizar los metodos, por ejemplo, toLocaleString
+	const date = new Date(data.data.time);
+	//Y aqui tenemos la fecha que nos devuelve la API formateada en España con los datos que queremos en el formato adecuado
+	const formattedDate = date.toLocaleString("es-ES", {
+		weekday: "long",
+		year: "numeric",
+		month: "long",
+		day: "numeric",
+		hour: "numeric",
+		minute: "numeric",
+	});
+	container.innerHTML = `
+  
+    <p id="Temperatura">${data.data.values.temperature}º</p>
+    <p id="CountryName">${data.location.name}</p>
+  `;
+	container2.innerHTML = `
+<div style="width: 100%"><iframe width="100%" height="600" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=${data.location.lat},${data.location.lon}+(Proyecto%20Vite)&amp;t=&amp;z=15&amp;ie=UTF8&amp;iwloc=B&amp;output=embed">`;
+};
 const Main = () => {
 	document.querySelector(".divisor4").innerHTML = HTMLdeInfo();
 	document.querySelector(".divisor3").innerHTML = HTMLdeMapa();
+	getRealTimeData("Belize");
+	//Recuperamos el input y el boton
+	const botones = document.querySelectorAll(".SoyONoSoyUnBoton");
+	const cuandoSeHaceClick = function (evento) {
+		// Recuerda, this es el elemento
+		getRealTimeData(this.innerText);
+
+		// Podemos cambiar cualquier cosa, p.ej. el estilo
+	};
+	// botones es un arreglo así que lo recorremos
+	botones.forEach((boton) => {
+		//Agregar listener
+		boton.addEventListener("click", cuandoSeHaceClick);
+	});
 };
 
 export default Main;
-//https://www.google.com/maps/@40.4190137,-3.7179958,14z?entry=ttu
-//<div style="width: 100%"><iframe width="100%" height="600" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=es&amp;q=Les%20Rambles,%201%20Barcelona,%20Spain+(Mi%20nombre%20de%20egocios)&amp;t=&amp;z=15&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"><a href="https://www.gps.ie/car-satnav-gps/">GPS devices</a></iframe></div>
--- main.js
+++ main.js
@@ -3,7 +3,6 @@ import Header from "./Header/Header.js";
 import Nav from "./Nav/Nav.js";
 import Main from "./Main/Main.js";
 import Footer from "./Footer/Footer.js";
-import Thermometer from "./Thermometer2/Thermometer.js";
 
 Header();
 Nav();
